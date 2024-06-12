43eb3af625306b9d736ed4e166b22cb131e4c5d1
Mazen Amr <160724290+mazenamr07@users.noreply.github.com>
2024-06-12 17:48:33 +0300

    Update Added replayability with clearing the console Organized the functions more Added the game in its own function

--- include/util.h
+++ include/util.h
@@ -2,14 +2,24 @@
 #include <algorithm>
 #include <vector>
 
+// For erasing the console
+#include <iostream>
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
 using namespace std;
 
 #ifndef TIC_TAC_TOE_FUNCTIONS_H
 #define TIC_TAC_TOE_FUNCTIONS_H
 
 bool choiceCheck(const string& choice, const vector<string>& arr);
-void displayArr(vector<string> arr);
+void displayArr(vector<string> arr, string playerCharacter);
 string strUpp(string str);
 bool checkWin(const vector<string>& ticArr, const string& character);
+void runGame(string playerMode, string playerCharacter);
+void clearConsole();
 
 #endif //TIC_TAC_TOE_FUNCTIONS_H
--- src/util.cpp
+++ src/util.cpp
@@ -4,7 +4,9 @@ bool choiceCheck(const string& choice, const vector<string>& arr) {
     return any_of(arr.begin(), arr.end(), [choice] (const string& i) {return i == choice;});
 }
 
-void displayArr(vector<string> arr) {
+void displayArr(vector<string> arr, string playerCharacter) {
+    playerCharacter = (playerCharacter == "X") ? "O" : "X";
+
     cout << "===========\n";
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
@@ -15,8 +17,9 @@ void displayArr(vector<string> arr) {
         }
         cout << endl;
         if (i == 2) {
-            cout << "===========\n"
-                    "->";
+            cout << "===========\n";
+            printf("%s's turn ->", playerCharacter.c_str());
+
         }
     }
 }
@@ -41,4 +44,75 @@ bool checkWin(const vector<string>& ticArr, const string& character) {
         }
     }
     return false;
+}
+
+void runGame(string playerMode, string playerCharacter) {
+    if (playerMode == "1") { // Another Player
+        cout << endl << "Okay now to play choose a number from 1 - 9";
+        cout << endl << "===========\n"
+                        " 1 | 2 | 3 \n"
+                        " 4 | 5 | 6 \n"
+                        " 7 | 8 | 9 \n"
+                        "===========\n";
+        printf("%s's turn ->", playerCharacter.c_str());
+
+        vector<string> ticArr = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        int counter = 0;
+
+        while (true) {
+            string slotPlace;
+            getline(cin, slotPlace);
+
+            while (!choiceCheck(slotPlace, ticArr)) {
+                printf("%s must enter a number from 1 - 9\n"
+                       "->", playerCharacter.c_str());
+                getline(cin, slotPlace);
+            }
+
+            replace(ticArr.begin(), ticArr.end(), slotPlace, playerCharacter);
+            displayArr(ticArr, playerCharacter);
+            counter++;
+
+            // breaking area
+            if (checkWin(ticArr, playerCharacter)) {
+                printf("\r%s Won!     \n", playerCharacter.c_str());
+                break;
+            }
+            if (counter == 9) {
+                cout << "\rIt's a Draw!     \n";
+                break;
+            }
+            playerCharacter = (playerCharacter == "X") ? "O" : "X";
+        }
+    }
+    else { // Computer
+
+    }
+}
+
+void clearConsole() {
+    #ifdef _WIN32
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        COORD coordScreen = {0, 0};
+        DWORD cCharsWritten;
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+        DWORD dwConSize;
+
+        if (!GetConsoleScreenBufferInfo(hConsole, &csbi)) {
+            return;
+        }
+        dwConSize = csbi.dwSize.X * csbi.dwSize.Y;
+
+        if (!FillConsoleOutputCharacter(hConsole, (TCHAR) ' ', dwConSize, coordScreen, &cCharsWritten)) {
+            return;
+        }
+
+        if (!FillConsoleOutputAttribute(hConsole, csbi.wAttributes, dwConSize, coordScreen, &cCharsWritten)) {
+            return;
+        }
+
+        SetConsoleCursorPosition(hConsole, coordScreen);
+    #else
+        std::cout << "\033[2J\033[1;1H";
+    #endif
 }
