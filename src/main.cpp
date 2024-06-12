b9ee74d308fa2656d440c4d08bca972f9a4b3a16
Mazen Amr <160724290+mazenamr07@users.noreply.github.com>
2024-06-12 17:48:33 +0300

    Update Added replayability with clearing the console Organized the functions more Added the game in its own function

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,63 @@
 // Things to be done:
-// 1. Replaying method needs some love
 // 3. AI Opponent
 
 #include "../include/util.h"
 
 int main() {
+    string playerMode, exitChoice, playerCharacter = "X";
+    vector<string> playerChoices = {"1", "2"};
+    vector<string> Characters = {"X", "O"};
+
     while (true) {
         cout << "Tic Tac Toe 1.0\n"
-                "To play choose a character X or O\n"
-                "===========\n";
-        cout << " 1 | 2 | 3 \n"
-                " 4 | 5 | 6 \n"
-                " 7 | 8 | 9 \n"
-                "===========\n"
+                "To play choose a mode:\n"
+                "1- Against a Player\n"
+                "2- Against Computer\n"
                 "->";
-        string character;
-        getline(cin, character);
-        character = strUpp(character);
 
-        vector<string> charArr = {"X", "O"};
-        while (!choiceCheck(character, charArr)) {
-            cout << "Wrong Character! Please enter X or O:\n"
+        getline(cin, playerMode);
+        while (!choiceCheck(playerMode, playerChoices)) {
+            cout << "Please choose a correct mode:\n"
                     "->";
-            getline(cin, character);
-            character = strUpp(character);
+            getline(cin, playerMode);
         }
 
-        cout << "Okay now choose a number from 1 to 9:\n"
-                "->";
-        vector<string> ticArr = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
-        int counter = 0;
+        // if player chooses a computer he should get levels in the future
 
-        while (true) {
-            string choice;
-            getline(cin, choice);
+        if (playerMode == "2") { // Computer
+            cout << endl << "Please enter X or O:\n"
+                            "->";
 
-            while (!choiceCheck(choice, ticArr)) {
-                cout << "Wrong Number! Please enter a number from 1 to 9:\n"
+            getline(cin, playerCharacter);
+            playerCharacter = strUpp(playerCharacter);
+            while (!choiceCheck(playerCharacter, Characters)) {
+                cout << "Please choose either X or O:\n"
                         "->";
-                getline(cin, choice);
+                getline(cin, playerCharacter);
+                playerCharacter = strUpp(playerCharacter);
             }
-
-            replace(ticArr.begin(), ticArr.end(), choice, character);
-            displayArr(ticArr);
-            counter++;
-
-            // breaking area
-            if (checkWin(ticArr, character)) {
-                cout << character << " Won!\n";
-                break;
-            }
-            if (counter == 9) {
-                cout << "It's a Draw!\n";
-                break;
-            }
-
-            character = (character == "X") ? "O" : "X";
         }
 
-        cout << "\nWanna play again?\n"
-                "Enter 1 to play again,\n"
-                "anything else to exit\n"
-                "->";
+        runGame(playerMode, playerCharacter);
 
-        string endChoice;
-        getline(cin, endChoice);
+        cout << endl << "Wanna play again?\n"
+                        "1) Yes, Play again\n"
+                        "2) No, Quit game\n"
+                        "->";
 
-        if (endChoice != "1") {
-            break;
+        getline(cin, exitChoice);
+        while (!choiceCheck(exitChoice, playerChoices)) {
+            cout << "Please choose a correct choice:\n"
+                    "->";
+            getline(cin, exitChoice);
+        }
+
+        if (exitChoice == "1") {
+            clearConsole();
+        }
+        else {
+            cout << endl << "Bye-bye!";
+            return 0;
         }
     }
-    return 0;
 }
